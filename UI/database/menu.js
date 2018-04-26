e46a369469d4a38309571f37980b3c990fd4e3f9
emekafredy <chinedufrderick@gmail.com>
2018-04-26 16:39:01 +0100

    Add Api endpoint to get and set menu

---
+++ UI/database/menu.js
@@ -0,0 +1,33 @@
+const menu = [
+  {
+    mealTitle: 'African Salad',
+    description: 'Prepared with Fish',
+    price: 1100,
+    imageUrl: 'https://s3.amazonaws.com/www.dealdey.com/system/deals/images/57137/original/Productimage---Glorious_J_Treat-_AB2543.jpg?1427788281',
+    category: 'African'
+  },
+  {
+    mealTitle: 'Egusi Soup',
+    description: 'Egusi Soup prepared with Chicken and stockfish',
+    price: 1000,
+    imageUrl: 'http://allnigerianfoods.com/wp-content/uploads/2015/02/egusi-soup1.jpg',
+    category: 'African'
+  },
+  {
+    mealTitle: 'Spaghetti',
+    description: 'Prepared with Fish',
+    price: 1100,
+    imageUrl: 'https://s3.amazonaws.com/www.dealdey.com/system/deals/images/57137/original/Productimage---Glorious_J_Treat-_AB2543.jpg?1427788281',
+    category: 'Foreign'
+  },
+  {
+    mealTitle: 'Noodless',
+    description: 'prepared with Chicken',
+    price: 1000,
+    imageUrl: 'http://allnigerianfoods.com/wp-content/uploads/2015/02/egusi-soup1.jpg',
+    category: 'Foreign'
+  }
+];
+
+
+export default menu;
