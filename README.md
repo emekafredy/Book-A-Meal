c098548120d5556e94713018796c60b072527183
emekafredy <chinedufrderick@gmail.com>
2018-05-09 02:01:01 +0100

    Set up endpoints to use database

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ UI Template: https://emekafredy.github.io/Book-A-Meal/UI
 [![Build Status](https://travis-ci.org/emekafredy/Book-A-Meal.svg)](https://travis-ci.org/emekafredy/Book-A-Meal)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
+[![Coverage Status](https://coveralls.io/repos/github/emekafredy/Book-A-Meal/badge.svg)](https://coveralls.io/github/emekafredy/Book-A-Meal)
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,30 +1,13 @@
-import Meal from '../controllers/mealController';
-import Menu from '../controllers/menuController';
-import Order from './../controllers/orderController';
-import User from '../controllers/userController';
-
-import UserMiddleware from '../middlewares/user';
+import mealRoutes from '../routes/mealRoutes';
+import menuRoutes from '../routes/menuRoutes';
+import orderRoutes from '../routes/orderRoutes';
+import userRoutes from '../routes/userRoutes';
 
 const routes = (app) => {
-  // MEALS
-  app.get('/api/v1/meals', UserMiddleware.getUser, UserMiddleware.checkAdmin, Meal.getMeals);
-  app.post('/api/v1/meals', UserMiddleware.getUser, UserMiddleware.checkAdmin, Meal.addMeal);
-  app.put('/api/v1/meals/:id', UserMiddleware.getUser, UserMiddleware.checkAdmin, Meal.updateMeal);
-  app.delete('/api/v1/meals/:id', UserMiddleware.getUser, UserMiddleware.checkAdmin, Meal.deleteMeal);
-
-  // MENU
-  app.post('/api/v1/menu', UserMiddleware.getUser, UserMiddleware.checkAdmin, Menu.setMenu);
-  app.get('/api/v1/menu', UserMiddleware.getUser, Menu.getMenu);
-
-  // ORDERS
-  app.post('/api/v1/orders', UserMiddleware.getUser, Order.makeOrder);
-  app.get('/api/v1/orders', UserMiddleware.getUser, UserMiddleware.checkAdmin, Order.getOrders);
-  app.put('/api/v1/orders/:id', UserMiddleware.getUser, Order.updateOrder);
-  app.delete('/api/v1/orders/:id', UserMiddleware.getUser, UserMiddleware.checkUser, Order.cancelOrder);
-
-  // USERS
-  app.post('/api/v1/auth/signup', User.userSignUp);
-  app.post('/api/v1/auth/login', User.userLogin);
+  mealRoutes(app);
+  menuRoutes(app);
+  orderRoutes(app);
+  userRoutes(app);
 };
 
 export default routes;
